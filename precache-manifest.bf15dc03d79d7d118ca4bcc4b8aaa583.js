0df093d4d934466830b8ce02153cea0d4e1dc520
AayushKumar176 <aayushdbg2002@gmail.com>
2022-09-14 00:24:32 +0530

    Updates

---
+++ precache-manifest.bf15dc03d79d7d118ca4bcc4b8aaa583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e143511a254ef3d8c29c",
+    "url": "/Textutils/static/css/main.fb3c5eb3.chunk.css"
+  },
+  {
+    "revision": "e143511a254ef3d8c29c",
+    "url": "/Textutils/static/js/main.e143511a.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "2ca4ff569050be884470",
+    "url": "/Textutils/static/js/runtime~main.2ca4ff56.js"
+  },
+  {
+    "revision": "d0e98f0c50ac79c512fed247cd6c6726",
+    "url": "/Textutils/index.html"
+  }
+];
